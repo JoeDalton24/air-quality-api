df7f49f0bb15ab55d24ef98fdd0b9e6edcd7e328
Ibrahima Diama KA <ibrahima.ka@tooshare.com>
2022-11-30 23:13:44 +0000

    fix routes

--- src/routes/airRoutes.js
+++ src/routes/airRoutes.js
@@ -33,17 +33,4 @@ const router = express.Router();
  */
 router.get("/city_air_quality", airQualityController);
 
-/**
- * @openapi
- * /healthcheck:
- *  get:
- *     tags:
- *     - Healthcheck
- *     description: Responds if the app is up and running
- *     responses:
- *       200:
- *         description: App is up and running
- */
-router.get("/healthcheck", (req, res) => res.sendStatus(200));
-
 export default router;
